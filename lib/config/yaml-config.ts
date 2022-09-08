b0e3f6c56f06e590393326c3e742d6f924334632
Jason Song <jasonsoft.net@gmail.com>
2022-09-08 19:48:17 +0800

    Fix: Update YAML config

--- lib/config/yaml-config.ts
+++ lib/config/yaml-config.ts
@@ -12,12 +12,9 @@ export interface YamlConfigOptions {
  * Use a configuration file in YAML format
  * Added by Jason.Song (成长的小猪) on 2022/09/08 15:42:31
  */
-export default function YamlConfig(options?: YamlConfigOptions) {
+export default function YamlConfig() {
   const fileName = `${process.env.NODE_ENV || 'development'}.yaml`;
-  const path =
-    options && options.basePath
-      ? resolve(options.basePath, fileName)
-      : resolve(fileName);
+  const path = resolve(fileName);
   return yaml.load(readFileSync(path, { encoding: 'utf-8' })) as Record<
     string,
     any
