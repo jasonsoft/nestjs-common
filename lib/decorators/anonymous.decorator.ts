a549115235fb8e13f7d3e49e488cb3fb7494024c
Jason Song <jasonsoft.net@gmail.com>
2022-09-23 14:32:44 +0800

    Update note

--- lib/decorators/anonymous.decorator.ts
+++ lib/decorators/anonymous.decorator.ts
@@ -2,7 +2,7 @@ import { SetMetadata } from '@nestjs/common';
 import { ANONYMOUS_KEY } from '../constants';
 
 /**
- * 匿名访问
+ * Anonymous access, bypass authorization verification
  * Added by Jason.Song on 2021/12/07 15:02:45
  */
 export const Anonymous = () => SetMetadata(ANONYMOUS_KEY, true);
