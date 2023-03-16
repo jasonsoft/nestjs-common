b2af672400e95b8d43b37335deed3273b1ff7c60
Jason Song <jasonsoft.net@gmail.com>
2023-03-16 23:49:12 +0800

    Add uuid

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,5 @@
 export * from './shared';
 export * from './convert-operator';
 export * from './parse-header';
+export * from './cast-to-int';
+export * from './uuid';
---
+++ lib/utils/uuid.ts
@@ -0,0 +1,7 @@
+import { v4 } from 'uuid';
+
+/**
+ * Build UUID
+ * Added by Jason.Song (成长的小猪) on 2023/03/16 23:46:29
+ */
+export const uuid = (): string => v4();
